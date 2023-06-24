2fbabe046c88c691c06ad348b152b494b7a7b829
zahidnafi <92217342+zahidnafi@users.noreply.github.com>
2023-06-24 22:56:31 +0700

    update endpoint

--- react/movie-database/src/utils/constants/endpoints.js
+++ react/movie-database/src/utils/constants/endpoints.js
@@ -3,9 +3,9 @@ const API_KEY = process.env.REACT_APP_API_KEY
 const BASE_URL = "https://api.themoviedb.org/3";
 
 const ENDPOINTS = {
-    POPULAR: `${BASE_URL}/movie/popular/?api_key=${API_KEY}`,
-    TOP_RATED: `${BASE_URL}/movie/top_rated/?api_key=${API_KEY}`,
-    NOW_PLAYING: `${BASE_URL}/movie/now_playing/?api_key=${API_KEY}`
+    POPULAR: `${BASE_URL}/movie/popular?api_key=${API_KEY}`,
+    TOP_RATED: `${BASE_URL}/movie/top_rated?api_key=${API_KEY}`,
+    NOW_PLAYING: `${BASE_URL}/movie/now_playing?api_key=${API_KEY}`
 
 }
 
